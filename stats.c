10dcc5edbff875bc4e18a17926f930f077574af0
Sai Shubham Biswal <biswalsaishubham@gmail.com>
2025-06-29 11:10:03 -0600

    second commit

---
+++ stats.c
@@ -0,0 +1,69 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.c> 
+ * @brief c file for assessment c1m1
+ *
+ *
+ * @author Sai Shubham Biswal
+ * @date 27/06/2025
+ *
+ */
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* Other Variable Declarations Go Here */
+int max=test[0];
+int min=test[0];
+  /* Statistics and Printing Functions Go Here */
+int find_maximum(unsigned char test[],int SIZE){
+  
+}
+
+int find_minimum(unsigned char test[],int SIZE){
+  
+}
+
+int find_mean(unsigned char test[],int SIZE){
+  
+}
+
+int sort_array(unsigned char test[],int SIZE){
+  
+}
+
+int find_median(unsigned char test[],int SIZE){
+  
+}
+
+void print_statistics(){
+  
+}
+
+void print_array(int test[], int SIZE){
+  
+}
+
+}
+
+/* Add other Implementation File Code Here */
---
+++ stats.h
@@ -0,0 +1,123 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file C1M1 
+ * @brief Program to see the analytics of given data 
+ *
+ * Program to analyaze an array of unsigned char data items and return 
+ * analytics on maximum, minimum, mean and median of the data. Also 
+ * reorder this data set from large to small.
+ *
+ * @author Sai Shubham Biswal
+ * @date 29/06/2025
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief Finds the maximum number in given array
+ *
+ * This function takes two inputs as 'Array', and the 'Size of the array'. After the
+ * operations inside the function, it returns the maximum value of the array, given
+ * as output.
+ *
+ * @param test This is the formal parameter name, which takes array as input
+ * @param SIZE Size of the given array as input
+ *
+ * @return The maximum value of the array
+ */
+int find_maximum(unsigned char test[],int SIZE);
+
+/**
+ * @brief Finds the minimum number in given array
+ *
+ * This function takes two inputs as 'Array', and the 'Size of the array'. After the
+ * operations inside the function, it returns the minimum value of the array, given
+ * as output.
+ *
+ * @param test This is the formal parameter name, which takes array as input
+ * @param SIZE Size of the given array as input
+ *
+ * @return The minimum value of the array
+ */
+int find_minimum(unsigned char test[],int SIZE);
+
+/**
+ * @brief Finds the mean of all number in given array
+ *
+ * This function takes two inputs as 'Array', and the 'Size of the array'. After the
+ * operations inside the function, it returns the mean value of numbers in the 
+ * array, given as output.
+ *
+ * @param test This is the formal parameter name, which takes array as input
+ * @param SIZE Size of the given array as input 
+ *
+ * @return The mean value of the array
+ */
+int find_mean(unsigned char test[],int SIZE);
+
+/**
+ * @brief Sorts all number in given array in descending order
+ *
+ * This function takes two inputs as 'Array', and the 'Size of the array'. After the
+ * operations inside the function, it sorts all numbers in the array in 
+ * descending order.
+ *
+ * @param test This is the formal parameter name, which takes array as input
+ * @param SIZE Size of the given array as input
+ *
+ * @return --
+ */
+int sort_array(unsigned char test[],int SIZE);
+
+/**
+ * @brief Finds the median of all number data in given array
+ *
+ * This function takes two inputs as 'Array', and the 'Size of the array'. After the
+ * operations inside the function, it returns the median value of numbers in the 
+ * array, given as output.
+ *
+ * @param test This is the formal parameter name, which takes array as input
+ * @param SIZE Size of the given array as input
+ *
+ * @return The median value of the array
+ */
+int find_median(unsigned char test[],int SIZE);
+
+/**
+ * @brief Prints all the statistical values of given array
+ *
+ * This function prints all the statistical values from the numbers inside the array. 
+ * Like Median, Mean, Maximum, Minimum.
+ *
+ *
+ * @return --
+ */
+int print_statistics();
+
+/**
+ * @brief Prints all number data in given array
+ *
+ * This function takes two inputs as 'Array', and the 'Size of the array'. After the
+ * operations inside the function, it prints all of numbers in the 
+ * array.
+ *
+ * @param test This is the formal parameter name, which takes array as input
+ * @param SIZE Size of the given array as input
+ *
+ * @return --
+ */
+int print_array(unsigned char test[],int SIZE);
+
+
